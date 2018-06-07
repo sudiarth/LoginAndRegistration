33230ebc44bcad745b7e2556c0281aa4c3fa9ea7
Kadek Sudiartha <sudieartha@gmail.com>
2018-06-07 11:22:48 +0800

    add route Login / Register

--- server.py
+++ server.py
@@ -15,4 +15,19 @@ def index():
 def authenticate():
     return render_template('authenticate.html')
 
+@app.route('/register', methods=['POST'])
+def register():
+    fullname = request.form['html_fullname']
+    email = request.form['html_email']
+    password = request.form['html_password']
+    confirm = request.form['html_confirm']
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['html_email']
+    password = request.form['html_password']
+
+    return redirect(url_for('index'))
+
 app.run(debug=True)
